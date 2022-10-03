16083d5ad779ecb702428b704635627c59382bf2
Alina <ram@sfedu.ru>
2022-10-03 14:54:54 +0300

    task_3

---
+++ lab_3.3/lab_3/Form1.Designer.cs
@@ -0,0 +1,271 @@
+﻿namespace lab_3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.v1 = new System.Windows.Forms.Label();
+            this.coord1 = new System.Windows.Forms.Label();
+            this.colorDialog1 = new System.Windows.Forms.ColorDialog();
+            this.select_1 = new System.Windows.Forms.Button();
+            this.select_2 = new System.Windows.Forms.Button();
+            this.coord2 = new System.Windows.Forms.Label();
+            this.v2 = new System.Windows.Forms.Label();
+            this.select_3 = new System.Windows.Forms.Button();
+            this.coord3 = new System.Windows.Forms.Label();
+            this.v3 = new System.Windows.Forms.Label();
+            this.rgb1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.rgb2 = new System.Windows.Forms.Label();
+            this.rgb3 = new System.Windows.Forms.Label();
+            this.fill_gradient = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.InactiveBorder;
+            this.panel1.Location = new System.Drawing.Point(13, 13);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(702, 456);
+            this.panel1.TabIndex = 0;
+            this.panel1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseDown);
+            // 
+            // v1
+            // 
+            this.v1.AutoSize = true;
+            this.v1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.v1.Location = new System.Drawing.Point(732, 38);
+            this.v1.Name = "v1";
+            this.v1.Size = new System.Drawing.Size(71, 20);
+            this.v1.TabIndex = 1;
+            this.v1.Text = "Vertex 1";
+            // 
+            // coord1
+            // 
+            this.coord1.AutoSize = true;
+            this.coord1.Location = new System.Drawing.Point(855, 125);
+            this.coord1.Name = "coord1";
+            this.coord1.Size = new System.Drawing.Size(42, 16);
+            this.coord1.TabIndex = 2;
+            this.coord1.Text = "X:    Y:";
+            // 
+            // select_1
+            // 
+            this.select_1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.select_1.Location = new System.Drawing.Point(736, 114);
+            this.select_1.Name = "select_1";
+            this.select_1.Size = new System.Drawing.Size(100, 38);
+            this.select_1.TabIndex = 3;
+            this.select_1.Text = "Select point";
+            this.select_1.UseVisualStyleBackColor = false;
+            this.select_1.Click += new System.EventHandler(this.select_1_Click);
+            // 
+            // select_2
+            // 
+            this.select_2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.select_2.Location = new System.Drawing.Point(736, 263);
+            this.select_2.Name = "select_2";
+            this.select_2.Size = new System.Drawing.Size(100, 38);
+            this.select_2.TabIndex = 6;
+            this.select_2.Text = "Select point";
+            this.select_2.UseVisualStyleBackColor = false;
+            this.select_2.Click += new System.EventHandler(this.select_2_Click);
+            // 
+            // coord2
+            // 
+            this.coord2.AutoSize = true;
+            this.coord2.Location = new System.Drawing.Point(855, 274);
+            this.coord2.Name = "coord2";
+            this.coord2.Size = new System.Drawing.Size(42, 16);
+            this.coord2.TabIndex = 5;
+            this.coord2.Text = "X:    Y:";
+            // 
+            // v2
+            // 
+            this.v2.AutoSize = true;
+            this.v2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.v2.Location = new System.Drawing.Point(732, 187);
+            this.v2.Name = "v2";
+            this.v2.Size = new System.Drawing.Size(71, 20);
+            this.v2.TabIndex = 4;
+            this.v2.Text = "Vertex 2";
+            // 
+            // select_3
+            // 
+            this.select_3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.select_3.Location = new System.Drawing.Point(736, 394);
+            this.select_3.Name = "select_3";
+            this.select_3.Size = new System.Drawing.Size(100, 38);
+            this.select_3.TabIndex = 9;
+            this.select_3.Text = "Select point";
+            this.select_3.UseVisualStyleBackColor = false;
+            this.select_3.Click += new System.EventHandler(this.select_3_Click);
+            // 
+            // coord3
+            // 
+            this.coord3.AutoSize = true;
+            this.coord3.Location = new System.Drawing.Point(855, 405);
+            this.coord3.Name = "coord3";
+            this.coord3.Size = new System.Drawing.Size(42, 16);
+            this.coord3.TabIndex = 8;
+            this.coord3.Text = "X:    Y:";
+            // 
+            // v3
+            // 
+            this.v3.AutoSize = true;
+            this.v3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.v3.Location = new System.Drawing.Point(732, 318);
+            this.v3.Name = "v3";
+            this.v3.Size = new System.Drawing.Size(71, 20);
+            this.v3.TabIndex = 7;
+            this.v3.Text = "Vertex 3";
+            // 
+            // rgb1
+            // 
+            this.rgb1.AutoSize = true;
+            this.rgb1.Location = new System.Drawing.Point(855, 81);
+            this.rgb1.Name = "rgb1";
+            this.rgb1.Size = new System.Drawing.Size(69, 16);
+            this.rgb1.TabIndex = 14;
+            this.rgb1.Text = "R:    G:    B:";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(736, 70);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(100, 38);
+            this.button1.TabIndex = 12;
+            this.button1.Text = "Select color";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(736, 219);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(100, 38);
+            this.button2.TabIndex = 13;
+            this.button2.Text = "Select color";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(736, 350);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(100, 38);
+            this.button3.TabIndex = 14;
+            this.button3.Text = "Select color";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // rgb2
+            // 
+            this.rgb2.AutoSize = true;
+            this.rgb2.Location = new System.Drawing.Point(855, 230);
+            this.rgb2.Name = "rgb2";
+            this.rgb2.Size = new System.Drawing.Size(69, 16);
+            this.rgb2.TabIndex = 15;
+            this.rgb2.Text = "R:    G:    B:";
+            // 
+            // rgb3
+            // 
+            this.rgb3.AutoSize = true;
+            this.rgb3.Location = new System.Drawing.Point(855, 361);
+            this.rgb3.Name = "rgb3";
+            this.rgb3.Size = new System.Drawing.Size(69, 16);
+            this.rgb3.TabIndex = 16;
+            this.rgb3.Text = "R:    G:    B:";
+            // 
+            // fill_gradient
+            // 
+            this.fill_gradient.BackColor = System.Drawing.Color.Cornsilk;
+            this.fill_gradient.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.fill_gradient.Location = new System.Drawing.Point(956, 426);
+            this.fill_gradient.Name = "fill_gradient";
+            this.fill_gradient.Size = new System.Drawing.Size(139, 43);
+            this.fill_gradient.TabIndex = 17;
+            this.fill_gradient.Text = "Fill gradient";
+            this.fill_gradient.UseVisualStyleBackColor = false;
+            this.fill_gradient.Click += new System.EventHandler(this.FillGradient_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.ClientSize = new System.Drawing.Size(1107, 481);
+            this.Controls.Add(this.fill_gradient);
+            this.Controls.Add(this.rgb3);
+            this.Controls.Add(this.rgb2);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.rgb1);
+            this.Controls.Add(this.select_3);
+            this.Controls.Add(this.coord3);
+            this.Controls.Add(this.v3);
+            this.Controls.Add(this.select_2);
+            this.Controls.Add(this.coord2);
+            this.Controls.Add(this.v2);
+            this.Controls.Add(this.select_1);
+            this.Controls.Add(this.coord1);
+            this.Controls.Add(this.v1);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "GradientTriangle";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label v1;
+        private System.Windows.Forms.Label coord1;
+        private System.Windows.Forms.ColorDialog colorDialog1;
+        private System.Windows.Forms.Button select_1;
+        private System.Windows.Forms.Button select_2;
+        private System.Windows.Forms.Label coord2;
+        private System.Windows.Forms.Label v2;
+        private System.Windows.Forms.Button select_3;
+        private System.Windows.Forms.Label coord3;
+        private System.Windows.Forms.Label v3;
+        private System.Windows.Forms.Label rgb1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label rgb2;
+        private System.Windows.Forms.Label rgb3;
+        private System.Windows.Forms.Button fill_gradient;
+    }
+}
+
---
+++ lab_3.3/lab_3/Form1.cs
@@ -0,0 +1,228 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace lab_3
+{
+
+    public partial class Form1 : Form
+    {
+        static void Swap<T>(ref T lhs, ref T rhs)
+        {
+            T temp;
+            temp = lhs;
+            lhs = rhs;
+            rhs = temp;
+        }
+
+        private SolidBrush Sb_op(SolidBrush sb1, SolidBrush sb2, string operation)
+        {
+            SolidBrush sb_new = new SolidBrush(Color.Black);
+            switch (operation)
+            {
+                case "-":
+                    sb_new = new SolidBrush(Color.FromArgb(Math.Max(sb1.Color.R - sb2.Color.R, 0),
+                        Math.Max(sb1.Color.G - sb2.Color.G, 0), Math.Max(sb1.Color.B - sb2.Color.B, 0)));
+                    break;
+                case "+":
+                    sb_new = new SolidBrush(Color.FromArgb(Math.Min(sb1.Color.R + sb2.Color.R, 255),
+                        Math.Min(sb1.Color.G + sb2.Color.G, 255), Math.Min(sb1.Color.B + sb2.Color.B, 255)));
+                    break;
+            }
+            
+            return sb_new;
+        }
+
+        private SolidBrush Sb_op_mult(SolidBrush sb1, float w)
+        {
+            return new SolidBrush(Color.FromArgb((int)(sb1.Color.R * w), (int)(sb1.Color.G * w), (int)(sb1.Color.B * w)));
+        }
+
+        private Rectangle TriangleBoundingBox(Point a, Point b, Point c)
+        {
+            Point top_left = new Point(Math.Min(Math.Min(a.X, b.X), c.X), Math.Min(Math.Min(a.Y, b.Y), c.Y));
+            Point bottom_right = new Point(Math.Max(Math.Max(a.X, b.X), c.X), Math.Max(Math.Max(a.Y, b.Y), c.Y));
+            Rectangle r = new Rectangle();
+            r.X = top_left.X;
+            r.Y = top_left.Y;
+            r.Width = bottom_right.X - top_left.X;
+            r.Height = bottom_right.Y - top_left.Y;
+
+            return r;
+        }
+        public Form1()
+        {
+            InitializeComponent();
+            gp = panel1.CreateGraphics();
+            rgb1.Text = "R: " + col1.Color.R + " G: " + col1.Color.G + " B: " + col1.Color.B;
+            rgb2.Text = "R: " + col2.Color.R + " G: " + col2.Color.G + " B: " + col2.Color.B;
+            rgb3.Text = "R: " + col3.Color.R + " G: " + col3.Color.G + " B: " + col3.Color.B;
+        }
+
+        Point p1;
+        Point p2;
+        Point p3;
+
+        bool getv1 = false;
+        bool getv2 = false;
+        bool getv3 = false;
+
+        Graphics gp;
+        Pen pen = new Pen(Brushes.Black, 1);
+
+        private void panel1_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (getv1)
+            {
+                p1 = new Point(e.X, e.Y);
+                coord1.Text = "X: " + p1.X + ", Y:" + p1.Y;
+            }
+
+            if (getv2)
+            {
+                p2 = new Point(e.X, e.Y);
+                coord2.Text = "X: " + p2.X + ", Y:" + p2.Y;
+                gp.DrawLine(pen, p1, p2);
+            }
+
+            if (getv3)
+            {
+                p3 = new Point(e.X, e.Y);
+                coord3.Text = "X: " + p3.X + ", Y:" + p3.Y;
+                gp.DrawLine(pen, p2, p3);
+                gp.DrawLine(pen, p3, p1);
+            }
+
+            panel1.Cursor = Cursors.Default;
+            getv1 = false;
+            getv2 = false;
+            getv3 = false;
+        }
+
+        private void select_1_Click(object sender, EventArgs e)
+        {
+            getv1 = true;
+            panel1.Cursor = Cursors.Cross;
+        }
+
+        private void select_2_Click(object sender, EventArgs e)
+        {
+            getv2 = true;
+            panel1.Cursor = Cursors.Cross;
+        }
+
+        private void select_3_Click(object sender, EventArgs e)
+        {
+            getv3 = true;
+            panel1.Cursor = Cursors.Cross;
+        }
+
+        SolidBrush col1 = new SolidBrush(Color.Green);
+        SolidBrush col2 = new SolidBrush(Color.Red);
+        SolidBrush col3 = new SolidBrush(Color.Blue);
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            ColorDialog MyDialog = new ColorDialog();
+            // Keeps the user from selecting a custom color.
+            MyDialog.AllowFullOpen = false;
+            // Allows the user to get help. (The default is false.)
+            MyDialog.ShowHelp = true;
+
+            // Update the text box color if the user clicks OK 
+            if (MyDialog.ShowDialog() == DialogResult.OK)
+            {
+                col1 = new SolidBrush(MyDialog.Color);
+                rgb1.Text = "R: " + MyDialog.Color.R + " G: " + MyDialog.Color.G + " B: " + MyDialog.Color.B;
+            }
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            ColorDialog MyDialog = new ColorDialog();
+            MyDialog.AllowFullOpen = false;
+            MyDialog.ShowHelp = true;
+
+            if (MyDialog.ShowDialog() == DialogResult.OK)
+            {
+                col2 = new SolidBrush(MyDialog.Color);
+                rgb2.Text = "R: " + MyDialog.Color.R + " G: " + MyDialog.Color.G + " B: " + MyDialog.Color.B;
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            ColorDialog MyDialog = new ColorDialog();
+            MyDialog.AllowFullOpen = false;
+            MyDialog.ShowHelp = true;
+
+            if (MyDialog.ShowDialog() == DialogResult.OK)
+            {
+                col3 = new SolidBrush(MyDialog.Color);
+                rgb3.Text = "R: " + MyDialog.Color.R + " G: " + MyDialog.Color.G + " B: " + MyDialog.Color.B;
+            }
+        }
+
+        
+        private void FillGradient_Click(object sender, EventArgs e)
+        {
+            if (p1.Y == p2.Y && p1.Y == p3.Y) return;
+
+            //храним старое значение точки р1, на вектор которой сдвигаем треугольник
+            Point old_p1 = new Point(p1.X, p1.Y);
+
+            //сдвигаем треугольник в начало координат
+            p2 = new Point(p2.X - p1.X, p2.Y - p1.Y);
+            p3 = new Point(p3.X - p1.X, p3.Y - p1.Y);
+            p1 = new Point(0, 0);
+
+            if (p3.Y == 0)
+            {
+                Swap(ref p3, ref p2);
+                Swap(ref col3, ref col2);
+            }
+
+            SolidBrush delta_col12 = Sb_op(col2, col1, "-");
+            SolidBrush delta_col13 = Sb_op(col3, col1, "-");
+
+
+            Rectangle rectangle = TriangleBoundingBox(p1, p2, p3);
+            Bitmap image = new Bitmap(rectangle.Width, rectangle.Height);
+
+            for (int y = rectangle.Top; y < rectangle.Bottom; y++)
+            {
+                for (int x = rectangle.Left; x < rectangle.Right; x++)
+                {
+                    //коэффициент-множитель для одной стороны треугольника
+                    float w1 = (float)(y * p3.X - x * p3.Y) / (float)(p2.Y * p3.X - p2.X * p3.Y);
+
+                    //если он в пределах единицы, точка может находиться  пределах треугольника
+                    if (w1 >= 0 && w1 <= 1)
+                    {
+                        //коэффициент для второй стороны
+                        float w2 = (float)(y - (w1 * p2.Y)) / (float)p3.Y;
+
+                        //если сумма коэффициентов в пределах единицы - точка в треугольнике
+                        if (w2 >= 0 && (w1 + w2) <= 1)
+                        {
+                            //вычислить цвет точки, сложив "цветовые вектора"
+                            Color res_color = Sb_op(Sb_op(col1, Sb_op_mult(delta_col12, w1), "+"), Sb_op_mult(delta_col13, w2), "+").Color;
+
+                            //установить точку
+                            image.SetPixel(x, y, res_color);
+                        }
+
+                    }
+                }
+            }
+            gp.Clear(panel1.BackColor);
+            gp.DrawImage(image, old_p1);
+        }
+    }
+}
