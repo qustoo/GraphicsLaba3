692342f9be61f87fb301197429706dcdd80d190a
Alex Zotkin <alex.z806@mail.ru>
2022-10-03 22:01:49 +0300

    задание 1

---
+++ Form1.Designer.cs
@@ -0,0 +1,124 @@
+﻿
+namespace task1_wf
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.fill = new System.Windows.Forms.Button();
+            this.draw_button = new System.Windows.Forms.Button();
+            this.fill_button = new System.Windows.Forms.Button();
+            this.pictureBox = new System.Windows.Forms.PictureBox();
+            this.border_button = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // fill
+            // 
+            this.fill.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.fill.Location = new System.Drawing.Point(13, 13);
+            this.fill.Name = "fill";
+            this.fill.Size = new System.Drawing.Size(186, 32);
+            this.fill.TabIndex = 0;
+            this.fill.Text = "Загрузить изображение";
+            this.fill.UseVisualStyleBackColor = true;
+            this.fill.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // draw_button
+            // 
+            this.draw_button.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.draw_button.Location = new System.Drawing.Point(205, 14);
+            this.draw_button.Name = "draw_button";
+            this.draw_button.Size = new System.Drawing.Size(95, 32);
+            this.draw_button.TabIndex = 2;
+            this.draw_button.Text = "Рисовать";
+            this.draw_button.UseVisualStyleBackColor = true;
+            this.draw_button.Click += new System.EventHandler(this.draw_button_Click);
+            // 
+            // fill_button
+            // 
+            this.fill_button.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.fill_button.Location = new System.Drawing.Point(306, 14);
+            this.fill_button.Name = "fill_button";
+            this.fill_button.Size = new System.Drawing.Size(95, 32);
+            this.fill_button.TabIndex = 4;
+            this.fill_button.Text = "Залить";
+            this.fill_button.UseVisualStyleBackColor = true;
+            this.fill_button.Click += new System.EventHandler(this.fill_button_Click);
+            // 
+            // pictureBox
+            // 
+            this.pictureBox.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pictureBox.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.pictureBox.Location = new System.Drawing.Point(13, 52);
+            this.pictureBox.Name = "pictureBox";
+            this.pictureBox.Size = new System.Drawing.Size(1131, 624);
+            this.pictureBox.TabIndex = 5;
+            this.pictureBox.TabStop = false;
+            this.pictureBox.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pictureBox_MouseDown);
+            this.pictureBox.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pictureBox_MouseMove);
+            // 
+            // border_button
+            // 
+            this.border_button.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.border_button.Location = new System.Drawing.Point(407, 14);
+            this.border_button.Name = "border_button";
+            this.border_button.Size = new System.Drawing.Size(160, 32);
+            this.border_button.TabIndex = 6;
+            this.border_button.Text = "Выделить границы";
+            this.border_button.UseVisualStyleBackColor = true;
+            this.border_button.Click += new System.EventHandler(this.border_button_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1156, 688);
+            this.Controls.Add(this.border_button);
+            this.Controls.Add(this.pictureBox);
+            this.Controls.Add(this.fill_button);
+            this.Controls.Add(this.draw_button);
+            this.Controls.Add(this.fill);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button fill;
+        private System.Windows.Forms.Button draw_button;
+        private System.Windows.Forms.Button fill_button;
+        private System.Windows.Forms.PictureBox pictureBox;
+        private System.Windows.Forms.Button border_button;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,270 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace task1_wf
+{
+    public partial class Form1 : Form
+    {
+        bool draw_mode = false;
+        bool fill_mode = false;
+        bool border_mode = false;
+        Color borderColor = Color.Black;
+        Bitmap texture = null;
+
+        public Form1()
+        {
+            InitializeComponent();
+            Bitmap bitmap = new Bitmap(pictureBox.ClientSize.Width, pictureBox.ClientSize.Height);
+            pictureBox.Image = bitmap;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog dlg = new OpenFileDialog();
+            dlg.DefaultExt = ".jpg";
+            dlg.Filter = "JPEG Files (*.jpeg)|*.jpeg|PNG Files (*.png)|*.png|JPG Files (*.jpg)|*.jpg|GIF Files (*.gif)|*.gif";
+            if (dlg.ShowDialog() == DialogResult.Cancel){
+                return;
+            }
+
+            else{
+                /*pictureBox.Image = new Bitmap(dlg.FileName);
+                pictureBox.Invalidate();*/
+                texture = new Bitmap(dlg.FileName);
+
+            }
+        }
+
+
+
+
+
+
+        private void draw_button_Click(object sender, EventArgs e){
+            draw_mode = !draw_mode;
+            fill_mode = false;
+            border_mode = false;
+            fill_button.BackColor = SystemColors.Control;
+            border_button.BackColor = SystemColors.Control;
+
+            if (draw_mode){
+                draw_button.BackColor = Color.DodgerBlue;
+            }
+            else{
+                draw_button.BackColor = SystemColors.Control;
+            }
+        }
+
+
+        private void fill_button_Click(object sender, EventArgs e){
+            fill_mode = !fill_mode;
+            draw_mode = false;
+            border_mode = false;
+            draw_button.BackColor = SystemColors.Control;
+            border_button.BackColor = SystemColors.Control;
+
+            if (fill_mode){
+                fill_button.BackColor = Color.DodgerBlue;
+            }
+            else{
+                fill_button.BackColor = SystemColors.Control;
+            }
+        }
+
+        private void border_button_Click(object sender, EventArgs e){
+            border_mode = !border_mode;
+            draw_mode = false;
+            fill_mode = false;
+            draw_button.BackColor = SystemColors.Control;
+            fill_button.BackColor = SystemColors.Control;
+
+            if (border_mode){
+                border_button.BackColor = Color.DodgerBlue;
+            }
+            else{
+                border_button.BackColor = SystemColors.Control;
+            }
+        }
+
+
+
+
+
+
+
+
+
+        Point draw_p;
+        private void pictureBox_MouseDown(object sender, MouseEventArgs e){
+            draw_p = e.Location;
+
+            if (fill_mode && texture == null){
+                fill_algorithm(draw_p, Color.Red);
+            }
+
+            else if (fill_mode && texture != null){
+                fill_texture_algorithm(draw_p, 0, 0);
+            }
+
+            else if (border_mode){
+                border_algorithm(draw_p);
+            }
+        }
+
+        private void pictureBox_MouseMove(object sender, MouseEventArgs e){
+            if (draw_mode && e.Button == MouseButtons.Left)
+            {
+                Graphics g = Graphics.FromImage(pictureBox.Image);
+                //g.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.AntiAlias;
+                g.DrawLine(new Pen(borderColor, 1), draw_p, e.Location);
+                draw_p = e.Location;
+                pictureBox.Invalidate();
+            }
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+        private void fill_algorithm(Point p, Color fill_color){
+            Bitmap bitmap = (Bitmap)pictureBox.Image;
+            Graphics g = Graphics.FromImage(bitmap);
+
+            if (bitmap.GetPixel(p.X, p.Y).ToArgb() == borderColor.ToArgb() || bitmap.GetPixel(p.X, p.Y).ToArgb() == fill_color.ToArgb()){
+                return;
+            }
+
+            while (bitmap.GetPixel(p.X - 1, p.Y).ToArgb() != borderColor.ToArgb()) {
+                p.X -= 1;
+            }
+            Point start = p;
+
+
+            while (bitmap.GetPixel(p.X + 1, p.Y).ToArgb() != borderColor.ToArgb()){
+                p.X += 1;
+            }
+            Point end = p;
+
+            g.DrawLine(new Pen(new SolidBrush(fill_color), 1), start, end);
+            pictureBox.Invalidate();
+
+            while (start.X != end.X){
+                fill_algorithm(new Point(start.X, start.Y + 1), fill_color);
+                fill_algorithm(new Point(start.X, start.Y - 1), fill_color);
+
+                start.X += 1;
+            }            
+        }
+
+
+        List<Point> fill_texture_points = new List<Point>();
+        private void fill_texture_algorithm(Point p, int texture_x, int texture_y)
+        {
+            Bitmap bitmap = (Bitmap)pictureBox.Image;
+            Graphics g = Graphics.FromImage(bitmap);
+
+            if ((bitmap.GetPixel(p.X, p.Y).ToArgb() == borderColor.ToArgb()) || (bitmap.GetPixel(p.X, p.Y).ToArgb() != 0)){
+                return;
+            }
+
+            while (bitmap.GetPixel(p.X - 1, p.Y).ToArgb() != borderColor.ToArgb()){
+                p.X -= 1;
+            }
+            Point start = p;
+
+            while (bitmap.GetPixel(p.X + 1, p.Y).ToArgb() != borderColor.ToArgb()){
+                p.X += 1;
+            }
+            Point end = p;
+
+            while (start.X != end.X){
+                g.DrawRectangle(new Pen(new SolidBrush(texture.GetPixel(texture_x, texture_y)), 1), new Rectangle(start.X, start.Y, 1, 1));
+                fill_texture_points.Add(start);
+                pictureBox.Invalidate();
+
+                fill_texture_algorithm(new Point(start.X, start.Y + 1), texture_x, (texture_y + 1) % texture.Height);
+                fill_texture_algorithm(new Point(start.X, start.Y - 1), texture_x, texture_y - 1 < 0 ? texture.Height - 1 : texture_y - 1);
+
+                start.X += 1;
+                texture_x = (texture_x + 1) % texture.Width;
+            }
+        }
+
+
+
+
+
+
+
+
+        private void border_algorithm(Point p){
+            Bitmap bitmap = (Bitmap)pictureBox.Image;
+            Graphics g = Graphics.FromImage(bitmap);
+            List<Point> points = new List<Point>();
+
+            while (bitmap.GetPixel(p.X, p.Y).ToArgb() != borderColor.ToArgb()){
+                p.X += 1;
+            }
+
+            border_algorithm_REC(p, bitmap, ref points);
+            
+            for(int i = 0; i < points.Count(); i++){
+                g.DrawRectangle(new Pen(Brushes.Red, 1), new Rectangle(points[i], new Size(1, 1)));
+            }
+            pictureBox.Invalidate();
+        }
+
+        public void border_algorithm_REC(Point p, Bitmap bitmap, ref List<Point> points){
+            if (!points.Contains(p)){
+                points.Add(p);
+
+                if (bitmap.GetPixel(p.X + 1, p.Y).ToArgb() == borderColor.ToArgb()){
+                    border_algorithm_REC(new Point(p.X + 1, p.Y), bitmap, ref points);
+                }
+                if (bitmap.GetPixel(p.X + 1, p.Y - 1).ToArgb() == borderColor.ToArgb()){
+                    border_algorithm_REC(new Point(p.X + 1, p.Y - 1), bitmap, ref points);
+                }
+                if (bitmap.GetPixel(p.X, p.Y - 1).ToArgb() == borderColor.ToArgb()){
+                    border_algorithm_REC(new Point(p.X, p.Y - 1), bitmap, ref points);
+                }
+                if (bitmap.GetPixel(p.X - 1, p.Y - 1).ToArgb() == borderColor.ToArgb()){
+                    border_algorithm_REC(new Point(p.X - 1, p.Y - 1), bitmap, ref points);
+                }
+                if (bitmap.GetPixel(p.X - 1, p.Y).ToArgb() == borderColor.ToArgb()){
+                    border_algorithm_REC(new Point(p.X - 1, p.Y), bitmap, ref points);
+                }
+                if (bitmap.GetPixel(p.X - 1, p.Y + 1).ToArgb() == borderColor.ToArgb())
+                {
+                    border_algorithm_REC(new Point(p.X - 1, p.Y + 1), bitmap, ref points);
+                }
+                if (bitmap.GetPixel(p.X, p.Y + 1).ToArgb() == borderColor.ToArgb())
+                {
+                    border_algorithm_REC(new Point(p.X, p.Y + 1), bitmap, ref points);
+                }
+                if (bitmap.GetPixel(p.X + 1, p.Y + 1).ToArgb() == borderColor.ToArgb())
+                {
+                    border_algorithm_REC(new Point(p.X + 1, p.Y + 1), bitmap, ref points);
+                }
+            }
+        }
+
+        
+    }
+
+      
+    }
